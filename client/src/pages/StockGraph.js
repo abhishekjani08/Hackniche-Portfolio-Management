e4b6c3c9ca46c3a928ba764282dec83669a98c7b
50_Abhishek Jani <88500027+abhishekjani08@users.noreply.github.com>
2024-02-17 20:50:48 +0530

    Update StockGraph.js

--- client/src/pages/StockGraph.js
+++ client/src/pages/StockGraph.js
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Table } from "antd";
+import axios from "axios";
 
 const StockGraph = () => {
-  return (
-    <div>StockGraph</div>
-  )
-}
+  const [stockData, setStockData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-export default StockGraph
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://financialmodelingprep.com/api/v3/stock/list?apikey=1ZH5AbyTIOQ4OhEKOqWZjea44O2NJgLn"
+        );
+        setStockData(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const columns = [
+    {
+      title: "Symbol",
+      dataIndex: "symbol",
+      key: "symbol",
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Price",
+      dataIndex: "price",
+      key: "price",
+    },
+    {
+      title: "Exchange",
+      dataIndex: "exchange",
+      key: "exchange",
+    },
+    {
+      title: "Exchange Short Name",
+      dataIndex: "exchangeShortName",
+      key: "exchangeShortName",
+    },
+    {
+      title: "Type",
+      dataIndex: "type",
+      key: "type",
+    },
+  ];
+
+  return (
+    <div>
+      <Table columns={columns} dataSource={stockData} loading={loading} />
+    </div>
+  );
+};
+
+export default StockGraph;
